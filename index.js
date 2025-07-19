518876cc43c5927008f0a104f291ea150f37af1d
rajkishork05 <kothiyalrajkishor1@gmail.com>
2025-07-19 16:15:01 +0530

    Removed .env file and added to .gitignore

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ app.use(express.static(path.join(__dirname, "./client/dist")))
 //route
 app.use("/api/v1/portfolio", require("./routes/contact_route"))
 
-app.get("*", function(req,res){
-    res.sendFile(path.join(__dirname, "./client/dist/index.html"))
-})
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/dist", "index.html"));
+});
 //port
 // const port = 8080;
 const PORT = process.env.PORT || 8080
